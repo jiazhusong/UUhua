76635fed8856eff39adefc7011c1eda339ea7575
jiazhusong <jiazhusong@sina.com>
2019-03-07 09:11:36 +0800

    111

--- src/pages/info/myInfo.vue
+++ src/pages/info/myInfo.vue
@@ -103,12 +103,12 @@
             </x-input>
             <x-input label-width='100' title='支付宝账号：' required ref='zfbzh' placeholder="请输入支付宝账号" v-model="bankObj.zfbzh">
             </x-input>
-            <x-input label-width='100' title='支付宝密码：'  required ref='zfbmm' placeholder="请输入支付宝密码" v-model="bankObj.zfbmm">
-            </x-input>
+            <!--<x-input label-width='100' title='支付宝密码：'  required ref='zfbmm' placeholder="请输入支付宝密码" v-model="bankObj.zfbmm">-->
+            <!--</x-input>-->
             <x-input label-width='100' title='淘宝账号：'  required ref='tbzh' placeholder="请输入淘宝账号" v-model="bankObj.tbzh">
             </x-input>
-            <x-input label-width='100' title='淘宝密码：' required ref='tbmm'  placeholder="请输入淘宝密码" v-model="bankObj.tbmm">
-            </x-input>
+            <!--<x-input label-width='100' title='淘宝密码：' required ref='tbmm'  placeholder="请输入淘宝密码" v-model="bankObj.tbmm">-->
+            <!--</x-input>-->
             <div style='height:1px;background: #D9D9D9; '></div>
             <x-button style='margin-top: 20px;' type="primary" @click.native='bankSubmit'>保存并下一步</x-button>
           </group>
@@ -225,9 +225,9 @@
               bankObj:{
                 bankAccount: "",
                 fhxx: "",
-                tbmm: "",
+                // tbmm: "",
                 tbzh: "",
-                zfbmm: "",
+                // zfbmm: "",
                 zfbzh: "",
                 bank:""
               },
